6b915c4a4b3a916ec80086126783b34bdd8d39e1
Csral <chaturyasral@gmail.com>
2026-03-04 11:36:50 +0000

    Added general protection fault (#GP int 13) Handler

--- src/kernel/idt/idt.asm
+++ src/kernel/idt/idt.asm
@@ -3,9 +3,11 @@ global idt_load
 
 ; Interrupt handler exports
 global idt_int_zero_handler
+global general_protection_fault
 
 ; 32-bit Kernel Handler functions
 extern int_zero
+extern int_gp_fault
 
 idt_load:
     push ebp
@@ -18,9 +20,36 @@ idt_load:
 
 idt_int_zero_handler:
 
+    push ebp
+    mov ebp, esp
     pushad
 
     call int_zero
 
     popad
+    mov esp, ebp
+    pop ebp
+
+    iret
+
+general_protection_fault:
+
+    push ebp
+    mov ebp, esp
+    pushad
+
+    push [ebp+4] ; err_code
+    push [ebp+8] ; EIP of faulting instruction
+    call int_gp_fault
+
+    add esp, 8
+    
+    cli
+    hlt
+
+    popad
+    mov esp, ebp
+    pop ebp
+    add esp, 4
+    
     iret
