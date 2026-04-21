37e1a91833c10063c6e1faf68175d2d7d19aa7bf
Csral <chaturyasral@gmail.com>
2026-04-21 21:42:43 +0000

    [+] memcmp

--- src/kernel/includes/memory/memory.h
+++ src/kernel/includes/memory/memory.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 void* memset(void* ptr, int c, size_t size);
+int memcmp(void* s1, void* s2, size_t size);
 void* memcpy(void* dst, void* src, size_t size);
 
 #endif
--- src/kernel/memory/memory.c
+++ src/kernel/memory/memory.c
@@ -11,6 +11,19 @@ void* memset(void* ptr, int c, size_t size) {
     return ptr;
 }
 
+int memcmp(void* s1, void* s2, size_t size) {
+
+    unsigned char* c1 = (unsigned char*) s1;
+    unsigned char* c2 = (unsigned char*) s2;
+
+    while (size-- > 0)
+        if (*c1++ != *c2++)
+            return c1[-1] < c2[-1] ? -1 : 1;
+
+    return 0;
+
+}
+
 void* memcpy(void* dst, void* src, size_t size) {
 
     for (size_t i = 0; i < size; i++)
