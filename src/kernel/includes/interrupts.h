dd21e7bd97ae06a56a852a5546a74ab7795f7904
Csral <chaturyasral@gmail.com>
2026-06-01 15:37:05 +0000

    [IDT]: Improved GP interrupt handler

--- src/kernel/includes/interrupts.h
+++ src/kernel/includes/interrupts.h
@@ -11,9 +11,17 @@ Define all interrupts here.
 
 #define _ACK_IRQ_EOI outb(0x20, 0x20);
 
+struct GP_Handler_stack {
+    unsigned long edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned long err_code;
+    unsigned long eip;
+    unsigned long cs;
+    unsigned long eflags;
+};
+
 void int_zero(void);
 void idt_invalid_opcode_fault_handler(void);
-void int_gp_fault(unsigned long address, uint32_t err_code);
+void int_gp_fault(struct GP_Handler_stack* stack_frame);
 void unhandled_interrupts_handler_basic(void);
 
 /* Interrupt handlers */
@@ -32,5 +40,8 @@ void no_interrupt_routine_handler(void);
 /* Unhandled interrupts */
 extern void unhandled_interrupts(void);
 extern void no_interrupt_routine(void); // IRQs
+
+/* Utils */
+extern unsigned short get_current_cs(void);
 
 #endif
