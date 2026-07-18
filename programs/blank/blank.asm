b73b552ba56cfcfa5e30136be5cf4cf3e0af08a7
Csral <me@csral.dev>
2026-07-18 22:04:13 +0000

    Tested ELF support and verified if the handler accounts for program header flags by triggering a PF

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -8,6 +8,20 @@ _start:
 
 label:
 
+    ; To test if flags are accounted for
+    ; we can try to write something into the code section
+    ; which is usually expected to be RX
+    
+    ; Since we expect this specific program to start
+    ; at 0x400000 and that is the address of _start
+    ; or its subsequent instructions, I think we can
+    ; try to write to that address.
+
+    ; mov eax, 0x400000
+    ; mov [eax], 0xA0
+
+    ; This should trigger a page fault.
+
     call get_key
     push eax
     mov eax, 4 ; sys_putchar
